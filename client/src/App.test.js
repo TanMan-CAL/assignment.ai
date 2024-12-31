21fe854d509cb67315772340ff6835dced4895ed
Tanmay Shah <70305211+TanMan-CAL@users.noreply.github.com>
2024-12-31 15:01:27 -0700

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom/client';
+
+
 
 test('renders learn react link', () => {
   render(<App />);
